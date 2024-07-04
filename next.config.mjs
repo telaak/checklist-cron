f38101b66e70422b4a43809f2b7850612a3a4749
Teemu L. <teemulaaks@gmail.com>
2024-07-05 00:30:28 +0300

    initial working version

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "standalone",
+  modularizeImports: {
+    "@mui/material": {
+      transform: "@mui/material/{{member}}",
+    },
+    "@mui/icons-material/?(((\\w*)?/?)*)": {
+      transform: "@mui/icons-material/{{ matches.[1] }}/{{member}}",
+    },
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+      config.resolve.fallback.tls = false;
+      config.resolve.fallback.net = false;
+      config.resolve.fallback.child_process = false;
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,114 +1,323 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import {
+  Box,
+  Button,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Tab,
+  Tooltip,
+} from "@mui/material";
+import { useEffect, useMemo, useState } from "react";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import parser from "cron-parser";
+import dayjs from "dayjs";
+import cronstrue from "cronstrue";
+import {
+  MRT_EditActionButtons,
+  type MRT_ColumnDef,
+  type MRT_Row,
+  type MRT_TableOptions,
+  useMaterialReactTable,
+  MRT_TableContainer,
+  MRT_TopToolbar,
+} from "material-react-table";
+import { nanoid } from "nanoid";
+import { CronJob } from "cron";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export type Checklist = {
+  cron: string;
+  task: string;
+  enabled: boolean;
+  lastChecked: Date;
+  id: string;
+};
+
+function setLocalstorage(newChecklist: Checklist[]) {
+  localStorage.setItem("checklist", JSON.stringify(newChecklist));
+}
+
+function getChecklist(): Checklist[] {
+  return typeof window !== "undefined"
+    ? JSON.parse(localStorage.getItem("checklist") as string)
+    : [];
+}
+
 export default function Home() {
+  useEffect(() => {
+    const cronjob = new CronJob(
+      "* * * * *",
+      () => {
+        const checklist = getChecklist();
+        setChecklist(checklist);
+        console.log("cronjob executed");
+      },
+      null,
+      true,
+      "Europe/Helsinki"
+    );
+  }, []);
+
+  const [value, setValue] = useState("1");
+  const [checklist, setChecklist] = useState<Checklist[]>([]);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
+  const markDone = (id: string) => {
+    const clone: Checklist[] = JSON.parse(JSON.stringify(checklist));
+    const listItem = clone.find((c) => c.id === id);
+    if (listItem) {
+      listItem.lastChecked = new Date();
+      setLocalstorage(clone);
+      setChecklist(clone);
+    }
+  };
+
+  useEffect(() => {
+    const checklist = getChecklist();
+    setChecklist(checklist);
+  }, []);
+
+  const columns = useMemo<MRT_ColumnDef<Checklist>[]>(
+    () => [
+      {
+        accessorKey: "task",
+        header: "Task",
+        muiEditTextFieldProps: {
+          required: true,
+        },
+        size: 50,
+      },
+      {
+        accessorKey: "cron",
+        header: "Cron",
+        muiEditTextFieldProps: {
+          required: true,
+        },
+        Cell({ row }) {
+          return (
+            <>{cronstrue.toString(row.original.cron, { verbose: true })}</>
+          );
+        },
+        size: 50,
+      },
+    ],
+    []
+  );
+
+  const handleCreateChecklistItem: MRT_TableOptions<Checklist>["onCreatingRowSave"] =
+    async ({ values, table }) => {
+      try {
+        const crons = cronstrue.toString(values.cron);
+        const checklist = getChecklist();
+        checklist.push({
+          task: values.task,
+          cron: values.cron,
+          lastChecked: new Date(),
+          enabled: true,
+          id: nanoid(),
+        });
+        setChecklist(checklist);
+        setLocalstorage(checklist);
+        table.setCreatingRow(null);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+  const handleSaveChecklistItem: MRT_TableOptions<Checklist>["onEditingRowSave"] =
+    async ({ values, table, row }) => {
+      try {
+        const crons = cronstrue.toString(values.cron);
+        const checklist = getChecklist();
+        const found = checklist.find((c) => c.id === row.original.id);
+        if (found) {
+          found.cron = values.cron;
+          found.task = values.task;
+          setChecklist(checklist);
+          setLocalstorage(checklist);
+          table.setEditingRow(null);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+  const openDeleteConfirmModal = (row: MRT_Row<Checklist>) => {
+    if (window.confirm(`Do you want to delete ${row.original.task}?`)) {
+      try {
+        const checklist = getChecklist();
+        const newChecklist = checklist.filter((c) => c.id !== row.original.id);
+        setChecklist(newChecklist);
+        setLocalstorage(newChecklist);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  const table = useMaterialReactTable({
+    columns,
+    data: checklist,
+    enablePagination: false,
+    createDisplayMode: "modal",
+    editDisplayMode: "modal",
+    enableEditing: true,
+    getRowId: (row) => row.id,
+    muiTableContainerProps: {
+      sx: {
+        minHeight: "500px",
+        maxWidth: "100%",
+      },
+    },
+    onCreatingRowSave: handleCreateChecklistItem,
+    onEditingRowSave: handleSaveChecklistItem,
+    renderCreateRowDialogContent: ({ table, row, internalEditComponents }) => (
+      <>
+        <DialogTitle variant="h5">Create task</DialogTitle>
+        <DialogContent
+          sx={{ display: "flex", flexDirection: "column", gap: "1rem" }}
+        >
+          {internalEditComponents}
+        </DialogContent>
+        <DialogActions>
+          <MRT_EditActionButtons variant="text" table={table} row={row} />
+        </DialogActions>
+      </>
+    ),
+    //optionally customize modal content
+    renderEditRowDialogContent: ({ table, row, internalEditComponents }) => (
+      <>
+        <DialogTitle variant="h5">Edit task</DialogTitle>
+        <DialogContent
+          sx={{ display: "flex", flexDirection: "column", gap: "1.5rem" }}
+        >
+          {internalEditComponents}
+        </DialogContent>
+        <DialogActions>
+          <MRT_EditActionButtons variant="text" table={table} row={row} />
+        </DialogActions>
+      </>
+    ),
+    renderRowActions: ({ row, table }) => (
+      <Box sx={{ display: "flex" }}>
+        <Tooltip title="Edit">
+          <IconButton onClick={() => table.setEditingRow(row)}>
+            <EditIcon />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Delete">
+          <IconButton color="error" onClick={() => openDeleteConfirmModal(row)}>
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      </Box>
+    ),
+    renderTopToolbarCustomActions: ({ table }) => (
+      <Button
+        variant="contained"
+        onClick={() => {
+          table.setCreatingRow(true);
+        }}
+      >
+        New Task
+      </Button>
+    ),
+  });
+
   return (
     <>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>Cron checklist</title>
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <TabList centered onChange={handleChange}>
+            <Tab label="Checklist" value="1" />
+            <Tab label="Settings" value="2" />
+          </TabList>
+        </Box>
+        <TabPanel
+          value="1"
+          sx={{
+            padding: 0,
+          }}
+        >
+          <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+            {checklist.map((checklist) => {
+              const parsed = parser.parseExpression(checklist.cron, {
+                currentDate: new Date(checklist.lastChecked),
+              });
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
+              const next = parsed.next().toDate();
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
+              const isDone = dayjs(next).isAfter(new Date());
 
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+              return (
+                <ListItem
+                  key={checklist.id}
+                  sx={
+                    {
+                      // display: isDone ? "none" : "flex",
+                    }
+                  }
+                >
+                  <ListItemAvatar>
+                    {isDone ? (
+                      <IconButton>
+                        <CheckBoxIcon />
+                      </IconButton>
+                    ) : (
+                      <IconButton
+                        onClick={(event) => {
+                          markDone(checklist.id);
+                        }}
+                      >
+                        <CheckBoxOutlineBlankIcon />
+                      </IconButton>
+                    )}
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={checklist.task}
+                    secondary={cronstrue.toString(checklist.cron, {
+                      verbose: true,
+                    })}
+                  />
+                </ListItem>
+              );
+            })}
+          </List>
+        </TabPanel>
+        <TabPanel
+          value="2"
+          sx={{
+            padding: 0,
+          }}
+        >
+          <MRT_TopToolbar table={table} />
+          <MRT_TableContainer table={table} />
+        </TabPanel>
+      </TabContext>
     </>
   );
 }
